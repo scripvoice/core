e36cbfd68d88b8968b861ea72d86327ec771c03f
Ravi Kumar K G <ravi.rk2406@gmail.com>
2023-02-25 10:39:30 +0000

    initial commit

---
+++ wire_providers.go
@@ -0,0 +1,14 @@
+package core
+
+import (
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+	)
+
+func ProvideMySqlContext() *MySqlContext {
+	return NewMySqlContext(viper.GetString("db_conn"))
+	}
+
+	var DependencySet = wire.NewSet(
+		ProvideMySqlContext,
+	)
