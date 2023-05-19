d921e0e75fbe24fa923aed4fade8e0ffcd0da2e0
Ravi Kumar K G <ravi.rk2406@gmail.com>
2023-05-19 14:44:12 +0000

    update di

--- wire_providers.go
+++ wire_providers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/wire"
 	config "github.com/scripvoice/core/config"
 	store "github.com/scripvoice/core/datastore"
+	infra "github.com/scripvoice/core/infrastructure"
 	"github.com/spf13/viper"
 )
 
@@ -21,5 +22,13 @@ func ProvideJwtAuth() *JwtAuth {
 	return NewJwtAuth()
 }
 
+func ProvideEventFactory() *infra.EventFactory {
+	return infra.NewEventFactory()
+}
+
+func ProvideDomainEventMediator(eventFactory *infra.EventFactory) *infra.DomainEventMediator {
+	return infra.NewDomainEventMediator(eventFactory)
+}
+
 var DependencySet = wire.NewSet(
-	ProvideMySqlContext, ProvideJwtAuth, ProvideDbContext)
+	ProvideMySqlContext, ProvideJwtAuth, ProvideDbContext, ProvideEventFactory, ProvideDomainEventMediator)
